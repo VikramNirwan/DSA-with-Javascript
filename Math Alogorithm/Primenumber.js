e3e34822731da4c305c49a2b909951567535fab2
Vikram Nirwan <vnirwan642@gmail.com>
2023-05-29 14:45:02 +0530

    Prime number

---
+++ Math Alogorithm/Primenumber.js
@@ -0,0 +1,22 @@
+// function isPrime(n) {
+//   if (n < 2) {
+//     return false;
+//   }
+//   for (let i = 2; i < n; i++) {
+//     if (n % i === 0) {
+//       return false;
+//     }
+//   }
+//   return true;
+// }
+
+// console.log(isPrime(1));
+// console.log(isPrime(4));
+// console.log(isPrime(5));
+
+// Big-O = O(n)
+
+// // We can replace the condition in the loop as
+//    i < Math.sqrt(n)
+
+// because Integers larger then the sqrt do not need to checkedd
