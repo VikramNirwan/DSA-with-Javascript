136a29ddd2179dd182378d60d2501e069e93f3c0
Vikram Nirwan <vnirwan642@gmail.com>
2023-05-29 13:30:12 +0530

    fibonacci question

---
+++ Math Alogorithm/fibonacci.js
@@ -0,0 +1,28 @@
+// find the first "n" elements of the fibonacci sequence of a no. "n".
+
+// function fibonacci(n) {
+//   const arr = [0, 1];
+//   for (let i = 2; i < n; i++) {
+//     let a = arr[arr.length - 1];
+//     let b = arr[arr.length - 2];
+//     let c = a + b;
+//     arr.push(c);
+//   }
+//   return arr;
+// }
+// console.log(fibonacci(2));
+// console.log(fibonacci(3));
+// console.log(fibonacci(7));
+
+//                  Or
+
+// function fibonacci(n) {
+//   const fibo = [0, 1];
+//   for (let i = 2; i < n; i++) {
+//     fibo[i] = fibo[i - 1] + fibo[i - 2];
+//   }
+//   return fibo;
+// }
+// console.log(fibonacci(2));
+// console.log(fibonacci(3));
+// console.log(fibonacci(7));
